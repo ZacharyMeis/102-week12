327fe6c0e76ffc129d00e9c628d2000209862eb1
ZacharyMeis <57931363+ZacharyMeis@users.noreply.github.com>
2019-11-18 22:38:11 -0700

    created LoadFile

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,3 +1,13 @@
 
 def PrintOutput(string):
     print("OUTPUT ", string)
+
+def LoadFile(file):
+    list_ = []
+    with open(file,"r") as f:
+        for line in f:
+            list_.append(line)
+    for i in range(len(list_)-1):
+        list_[i] = list_[i].strip('\n')
+    return list_
+            
